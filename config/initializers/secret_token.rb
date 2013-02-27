29db710b15dd6a02834d3fd8a6de0da71c665650
Niu Yameng <yameng.niu@rea-group.com>
2013-02-27 23:17:51 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ICube::Application.config.secret_token = 'e867c4d5ec9fe232fc8cc8bc275f51a8a296eb2d964c8726ad8e00eb1aa7f983e661aa357e25d0427a76d2461b4c2804e440a8884359f3b2a9aa7474e5a28105'
